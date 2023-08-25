c260297524bdcfb2bcf4513d9aee07f95dcb7baf
Nathan Peake <37817019+NathanPeake@users.noreply.github.com>
2023-08-25 17:28:07 +0100

    remove comments

--- src/KerbalPartManagerMod.cs
+++ src/KerbalPartManagerMod.cs
@@ -32,18 +32,6 @@ public class LegacyMode
     [HarmonyPrefix]
     public static bool PartsManagerPartsList_MarkDirty(PartsManagerPartsList __instance)
     {
-        //var _allParts = (Dictionary<IGGuid, IInteractivePart>)GameManager.Instance.Game.PartsManager.PartsList.GetType().GetField("_allParts", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(GameManager.Instance.Game.PartsManager.PartsList);
-        //ClearAllUI();
-        //try
-        //{
-        //    logger.Log($"LastPartID: {LastPartID}");
-        //    logger.Log($"_allParts: {_allParts.Keys}");
-        //    AddAllUIForPart(_allParts[LastPartID]);
-        //}
-        //catch (Exception ex)
-        //{
-        //    logger.Log($"{ex}");
-        //}
         if (BetterPartsManagerMod.AllowUpdate)
         {
             BetterPartsManagerMod.AllowUpdate = false;
